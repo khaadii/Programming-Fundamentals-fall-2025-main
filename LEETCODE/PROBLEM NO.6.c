a66b8206111c9076d7f4cd8796c1b164574ff64d
khaadii <khaaaaadiiiii@gmail.com>
2025-11-24 04:25:54 -0800

    Add files via upload

---
+++ LEETCODE/PROBLEM NO.6.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int num,div,i,j,left,right;
+	printf("\nEnter a number:  ");
+	scanf("%d",&num);
+	char ch[100];
+	sprintf(ch,"%d",num);
+	for(i=0;i<((strlen(ch))/2);i++)
+	{
+		sprintf(ch,"%d",num);
+		div=1;
+		for(j=1;j<strlen(ch);j++)
+		{
+			div=div*10;
+		}
+		left=num/div;
+		right=num%10;
+		if(left!=right)
+		{
+			printf("not palindrome");
+			return 0;
+		}
+		num=num/10;
+		sprintf(ch,"%d",num);
+		div=1;
+		for(j=1;j<strlen(ch);j++)
+		{
+			div=div*10;
+		}
+		num=num%div;
+	}
+	printf("\npalindrome number");
+	return 0;
+
+}
